c17001e6d65fc670e870389b0d06f45c9dbb421b
BarDanRob <117157760+BarDanRob@users.noreply.github.com>
2022-11-03 13:19:58 +0200

    Init Grid Set-Up from YouTube Tutorial
    Set up a Grid Scene with a grid generation basic system - plan to integrate with Tilesets later today.
    
    Next Up: Basic object placement in grid and movement.
    
    https://www.youtube.com/watch?v=kkAjpQAM-jE

---
+++ DADP Exam Project/Assets/Resources/Scripts/GridManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private int grid_width, grid_height;
+
+    [SerializeField]
+    private Tile tilePrefab;
+
+    [SerializeField]
+    private Camera Camera;
+
+    private Dictionary<Vector3, Tile> gridTiles;
+
+    public void GenerateGrid()
+    {
+        for(int x = 0; x<grid_width; x++)
+        {
+            for(int y = 0; y<grid_height; y++)
+            {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x}, {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+
+
+                gridTiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
+        Camera.transform.position = new Vector3((float)grid_width/2 -0.5f, (float)grid_height/2-0.5f, -10);
+    }
+
+
+    public Tile GetTileAtPosition(Vector2 position)
+    {
+        if(gridTiles.TryGetValue(position, out var tile)){
+            return tile;
+        }
+        return null;
+    }
+}
