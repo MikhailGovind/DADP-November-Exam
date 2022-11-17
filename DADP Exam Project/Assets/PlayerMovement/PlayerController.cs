c8c372e74d3bd990dd24cd0d30bcd90ef7a6e404
MikhailNamikaze <27miks12@gmail.com>
2022-11-17 23:04:57 +0200

    TestingObstacleMovement

--- DADP Exam Project/Assets/PlayerMovement/PlayerController.cs
+++ DADP Exam Project/Assets/PlayerMovement/PlayerController.cs
@@ -39,6 +39,17 @@ public class PlayerController : MonoBehaviour
 
     public bool playerAlive;
 
+    //object variables
+    private GameObject[] BlockObstacles;
+    private GameObject[] PushObstacles;
+    private GameObject[] BackPushObstacles;
+    private GameObject[] ExplodeObstacles;
+    private GameObject[] TeleportObstacles;
+
+    private bool ReadyToMove;
+
+
+
     [field: SerializeField]
     public UnitManager unitManager { get; private set; }
 
@@ -74,6 +85,13 @@ public class PlayerController : MonoBehaviour
         PlayerTile = GridManager.gridTiles[PlayerPosition];
         //signal = false;
         controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>()); //finding values for whenever the player avatar moves. Reading Vector2 values when performed
+
+        //obstacles have to have these  tags
+        BlockObstacles = GameObject.FindGameObjectsWithTag("BlockObstacles");
+        PushObstacles = GameObject.FindGameObjectsWithTag("PushObstacles");
+        BackPushObstacles = GameObject.FindGameObjectsWithTag("BackPushObstacles");
+        ExplodeObstacles = GameObject.FindGameObjectsWithTag("ExplodeObstacles");
+        TeleportObstacles = GameObject.FindGameObjectsWithTag("TeleportObstacles");
     }
 
     private void Move(Vector2 direction)
@@ -90,7 +108,20 @@ public class PlayerController : MonoBehaviour
 
                 animator.SetBool("isWalking", true); //set walking to true
             }
-        } 
+        }
+
+        if (direction.sqrMagnitude > 0.5)
+        {
+            if (ReadyToMove)
+            {
+                ReadyToMove = false;
+                objectMove(direction);
+            }
+        }
+        else
+        {
+            ReadyToMove = true;
+        }
 
         //set direction of sprite to movement direction 
         if (direction.x < 0) //left
@@ -147,5 +178,117 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Winner");
             unitManager.playerWin();
         }
+    }
+
+    public bool objectMove(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) < 0.5)
+        {
+            direction.x = 0;
+        }
+        else
+        {
+            direction.y = 0;
+        }
+        direction.Normalize();
+
+        //check if player is blocked.
+        if (Blocked(transform.position, direction))
+        {
+            return false;
+        }
+        else
+        {
+            transform.Translate(direction);
+            return true;
+        }
+    }
+
+    //interacting with the blocks and getting them to something here.
+    public bool Blocked(Vector3 position, Vector2 direction)
+    {
+        Vector2 newpos = new Vector2(position.x, position.y) + direction;
+        //Stactic block, doesn't move.
+        foreach (var obj in BlockObstacles)
+        {
+            if (obj.transform.position.x == newpos.x && obj.transform.position.y == newpos.y)
+            {
+                return true;
+            }
+        }
+
+        //Push block, supposed to move obe block at a time in the direction of the player
+        foreach (var objToPush in PushObstacles)
+        {
+            if (objToPush.transform.position.x == newpos.x && objToPush.transform.position.y == newpos.y)
+            {
+
+                DoObstacle objpush = objToPush.GetComponent<DoObstacle>();
+                if (objToPush && objpush.Move(direction))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        //Block destroys the player when they come in contact with it
+        foreach (var objEx in ExplodeObstacles)
+        {
+            if (objEx.transform.position.x == newpos.x && objEx.transform.position.y == newpos.y)
+            {
+
+                DoObstacle objexplode = objEx.GetComponent<DoObstacle>();
+                if (objEx && objexplode.Move(direction))
+                {
+                    gameObject.SetActive(false);
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        //This block pushes the player a space backwards.
+        foreach (var objToBackPush in BackPushObstacles)
+        {
+            if (objToBackPush.transform.position.x == newpos.x && objToBackPush.transform.position.y == newpos.y)
+            {
+
+                DoObstacle objbackpush = objToBackPush.GetComponent<DoObstacle>();
+                if (objToBackPush && objbackpush.Move(direction))
+                {
+                    transform.Translate(-1, -1, 0);
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        //This blocks teleports the player to new position on the grid.
+        foreach (var objToTeleport in TeleportObstacles)
+        {
+            if (objToTeleport.transform.position.x == newpos.x && objToTeleport.transform.position.y == newpos.y)
+            {
+
+                DoObstacle objteleport = objToTeleport.GetComponent<DoObstacle>();
+                if (objToTeleport && objteleport.Move(direction))
+                {
+                    transform.Translate(5, 5, 0);
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
