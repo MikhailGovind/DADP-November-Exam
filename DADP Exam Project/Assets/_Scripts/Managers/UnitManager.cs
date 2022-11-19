dbbebb667e6775a7fbae4d7218fee0d8de39fd73
MikhailNamikaze <27miks12@gmail.com>
2022-11-20 00:02:58 +0200

    Created_Levels
    Created all the levels
    Changed the story
    Added credits

--- DADP Exam Project/Assets/_Scripts/Managers/UnitManager.cs
+++ DADP Exam Project/Assets/_Scripts/Managers/UnitManager.cs
@@ -10,7 +10,10 @@ public class UnitManager : MonoBehaviour
     public EnemyController enemyController { get; private set; }
 
     [field: SerializeField]
-    /**/public EnemyController enemyController2 { get; private set; }
+    public EnemyController enemyController2 { get; private set; }
+
+    [field: SerializeField]
+    public EnemyController enemyController3 { get; private set; }
 
     [field: SerializeField]
     public PlayerController playerController { get; private set; }
@@ -42,7 +45,9 @@ public class UnitManager : MonoBehaviour
     {
         yield return new WaitForSecondsRealtime(time);
         enemyController.EnemyMove(playerController.PlayerPosition);
-        /**/enemyController2.EnemyMove(playerController.PlayerPosition);
+        enemyController2.EnemyMove(playerController.PlayerPosition);
+        enemyController3.EnemyMove(playerController.PlayerPosition);
+
         yield return null;
     }
 
